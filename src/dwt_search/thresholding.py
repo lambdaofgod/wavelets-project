0e2dfbabfd746d80bdb73247bee8b9e39f415e2f
lambdaofgod <bartczukkuba@gmail.com>
2017-06-26 18:28:20 +0200

    More appropriate parameter name for thresholding

--- src/dwt_search/thresholding.py
+++ src/dwt_search/thresholding.py
@@ -14,24 +14,24 @@ class ThresholdTransformer:
     n : int
         Number of wavelet coefficients to save
 
-    binary: bool, default=True
+    take_sign: bool, default=True
         Only use sign of thresholded values
 
     """
 
-    def __init__(self, n, binary=True):
+    def __init__(self, n, take_sign=True):
         self.n = n
-        self.binary = binary
+        self.take_sign = take_sign
 
     def fit(self, data):
         return
 
     def transform(self, data):
         """Apply thresholding for each data point"""
-        return [self.__thresholder(self.binary)(self.n, point) for point in data]
+        return [self.__thresholder(self.take_sign)(self.n, point) for point in data]
 
-    def __thresholder(self, binary):
-        if binary:
+    def __thresholder(self, take_sign):
+        if take_sign:
             return threshold_smallest_sign
         else:
             return threshold_smallest
